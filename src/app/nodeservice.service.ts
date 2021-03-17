c9b5f573d1b0ad75dfb376c20a014f9e463dafc2
bajajris <bajajris@sheridancollege.ca>
2021-03-17 17:24:16 -0400

    Added methods in service

--- src/app/nodeservice.service.ts
+++ src/app/nodeservice.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class NodeserviceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {  }
+  retrieveSchoolCovidSummary(){
+    return this.http.get('http://127.0.0.1:8888/schoolcovidsummary/', {});
+  }
+
+  retrieveSchoolCovidTesting(){
+    return this.http.get('http://127.0.0.1:8888/schoolcovidtesting/', {});
+  }
+
+  retrieveSchoolPartnersActiveCovid(){
+    return this.http.get('http://127.0.0.1:8888/schoolpartnersactivecovid/', {});
+  }
+
+  retrieveCasesByStatusAndPHU(){
+    return this.http.get('http://127.0.0.1:8888/casesbystatusandphu/', {});
+  }
+
+  retrieveOngoingOutbreaks(){
+    return this.http.get('http://127.0.0.1:8888/ongoingoutbreaks/', {});
+  }
 }
