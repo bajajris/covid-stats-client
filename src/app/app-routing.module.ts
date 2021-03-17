46875918f68710c2c51ea61180231b73073b02fc
bajajris <bajajris@sheridancollege.ca>
2021-03-17 15:01:19 -0400

    Added pages, modified app structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: '/page1',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'page1',
+    loadChildren: () => import('./pages/page1/page1.module').then( m => m.Page1PageModule)
+  },
+  {
+    path: 'page2',
+    loadChildren: () => import('./pages/page2/page2.module').then( m => m.Page2PageModule)
+  },
+  {
+    path: 'page3',
+    loadChildren: () => import('./pages/page3/page3.module').then( m => m.Page3PageModule)
   }
 ];
 
