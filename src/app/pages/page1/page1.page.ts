038d49e386c8da09568393874c367174659b2d52
bajajris <bajajris@sheridancollege.ca>
2021-03-17 23:33:14 -0400

    Added page 1 content

--- src/app/pages/page1/page1.page.ts
+++ src/app/pages/page1/page1.page.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NodeserviceService } from 'src/app/nodeservice.service';
 
 @Component({
   selector: 'app-page1',
@@ -6,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page1.page.scss'],
 })
 export class Page1Page implements OnInit {
+  title: string = "Schools COVID-19 Data"
+  covidSummary: any = []
+  covidTesting: any = []
+  covidPartnersActive: any = []
+  constructor(private nodeService: NodeserviceService) { }
 
-  constructor() { }
+  displaySchoolCovidSummary(){
+    this.nodeService.retrieveSchoolCovidSummary().subscribe(data=>{
+      this.covidSummary = data;
+      console.log(data);
+    },(err:HttpErrorResponse)=>{
+      console.log(err.message)
+    })
+    console.log(this.covidSummary)
+  }
 
+  displaySchoolCovidTesting(){
+    this.nodeService.retrieveSchoolCovidTesting().subscribe(data=>{
+      this.covidTesting = data;
+    },(err:HttpErrorResponse)=>{
+      console.log(err.message)
+    })
+    console.log(this.covidTesting)
+
+  }
+
+  displaySchoolPartnersActiveCovid(){
+    this.nodeService.retrieveSchoolPartnersActiveCovid().subscribe(data=>{
+      console.log(data)
+      this.covidPartnersActive = data;
+    },(err:HttpErrorResponse)=>{
+      console.log(err.message)
+    })
+    console.log(this.covidPartnersActive)
+  }
   ngOnInit() {
   }
 
