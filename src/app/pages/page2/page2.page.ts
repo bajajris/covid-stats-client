84fc5f2a11b2e1b141dcb3a2502ba055c721122f
bajajris <bajajris@sheridancollege.ca>
2021-03-17 23:53:12 -0400

    Added page 2 content

--- src/app/pages/page2/page2.page.ts
+++ src/app/pages/page2/page2.page.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NodeserviceService } from 'src/app/nodeservice.service';
 
 @Component({
   selector: 'app-page2',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page2Page implements OnInit {
 
-  constructor() { }
+  phu: any= []
+  constructor(private nodeService: NodeserviceService) { }
 
   ngOnInit() {
+    this.nodeService.retrieveCasesByStatusAndPHU().subscribe(data=>{
+      this.phu = data;
+      console.log(data);
+    },(err:HttpErrorResponse)=>{
+      console.log(err.message)
+    })
+    console.log(this.phu)
   }
 
 }
